a0ec861d34d0a3b4342f4d9cc3209c102ce1f546
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-11-03 16:27:44 +0000

    will use regex and string scan method to separate words, ignoring punctuation

--- sub-strings/sub_strings.rb
+++ sub-strings/sub_strings.rb
@@ -2,11 +2,11 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 
 def substrings(word, array_of_substrings)
-    split_word = word.split()
+    separate_words = words.scan("/\b\w+\b").map {|word| word.downcase}
     occurence_hash = Hash.new()
-    split_word.each do |word|
-        if word.downcase in array_of_substrings
-            occurence_hash[word] = array_of_substrings.count(word)
+    dictionary.each do |word|
+        if word in separate_words
+            occurence_hash[word] = separate_words.count(word)
         end
     end
     return occurence_hash
