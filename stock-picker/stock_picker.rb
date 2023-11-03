14a38ed4fd23a89651f554b988e66add0146efc0
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-11-03 18:21:43 +0000

    choose best days to buy and sell, and returns the index of those values from the stock provided

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -9,31 +9,16 @@ def stock_picker(stock)
             end
         end
     end
-    profit_days_array.each do |day_details|
-        puts "Buying price is #{day_details[1]}, selling price is #{day_details[2]}, total profit is #{day_details[0]}" 
-    end
-    all_profits = profit_days_array.select {day_details | day_details[0]}
-    puts all_profits
+    #profit_days_array.each do |day_details|
+    #    puts "Buying price is #{day_details[1]}, selling price is #{day_details[2]}, total profit is #{day_details[0]}" 
+    #end
+    all_profits = profit_days_array.map {|day_details| day_details[0]}
+    #p all_profits
+    best_days = profit_days_array.find { |day_details| day_details[0] == all_profits.max() }
+    #p best_days
+    return [stock.find_index(best_days[1]), stock.find_index(best_days[2])]
 end
 
 
 
-stock_picker([17,3,6,9,15,8,6,1,10])
-
-=begin
-best_days = []
-if stock.present?
-    cheapest_day = stock[0]
-    priciest_day = stock[0]
-    most_profit = 0
-while best_days.empty?
-    stock.each_with_index do |day_price, day|
-        if day_price < cheapest_day
-            cheapest_day = day_price
-        end
-        if day_price > priciest_day
-            priciest_day = day_price
-        end
-    end
-    if 
-=end
+p stock_picker([17,3,6,9,15,8,6,1,10])
