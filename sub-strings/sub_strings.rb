99dbdb8c9d34bbd66832713785c456ead1bb1999
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2023-11-03 17:37:46 +0000

    make code more concise, removed un-necessary lines

--- sub-strings/sub_strings.rb
+++ sub-strings/sub_strings.rb
@@ -2,8 +2,8 @@ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","o
 
 
 def substrings(word, array_of_substrings)
-    separate_words = word.scan(/\b\w+\b/).map {|input_word| input_word.downcase}
-    occurence_hash = Hash.new()
+    separate_words = word.scan(/\b\w+\b/).map(&:downcase)
+    occurence_hash = Hash.new(0)
     array_of_substrings.each do |array_word|
         if separate_words.include?(array_word)
             occurence_hash[array_word] = separate_words.count(array_word)
@@ -12,13 +12,11 @@ def substrings(word, array_of_substrings)
             if single_word.include?(array_word) && single_word.length != array_word.length
                 if occurence_hash.has_key?(array_word)
                     occurence_hash[array_word] += 1
-                else
-                    occurence_hash[array_word] = 1
                 end
             end
         end
     end
-    return occurence_hash
+    occurence_hash
 end
 
 puts substrings("Howdy howdy partner, sit down! How's it going mate?", dictionary)
