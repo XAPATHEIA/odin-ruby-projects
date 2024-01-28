a01c51bad437750f4902a9e6c82979849961f041
xapatheia <54541033+XAPATHEIA@users.noreply.github.com>
2024-01-28 21:02:57 +0000

    final commit of tutorial

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,13 +1,46 @@
 require 'csv'
-puts 'Event Manager Initialized!'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+
 
 # Checks if event_attendees.csv exists in cwd
 puts File.exist? "event_attendees.csv"
+
+civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+  
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'Event Manager Initialized!'
 
 
 contents = CSV.open(
@@ -16,10 +49,20 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
-  
-  puts "#{name} #{zipcode}"
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
+
+
